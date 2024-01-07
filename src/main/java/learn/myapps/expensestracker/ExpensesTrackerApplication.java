9171ff43c6b7b196768316d6382ce04a58f60e4c
Ravindra Reddy Ambati <ravindra.reddy.ambati@outlook.in>
2024-01-07 21:00:57 +0530

    initial commit

---
+++ src/main/java/learn/myapps/expensestracker/ExpensesTrackerApplication.java
@@ -0,0 +1,13 @@
+package learn.myapps.expensestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensesTrackerApplication.class, args);
+	}
+
+}
