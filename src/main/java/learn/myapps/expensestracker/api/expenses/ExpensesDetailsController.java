1fbe276d997cdeffcfa79ddba07650c7d3e0dd7c
Ravindra Reddy Ambati <ravindra.reddy.ambati@outlook.in>
2024-01-29 23:30:10 +0530

    updated the api calls

--- src/main/java/learn/myapps/expensestracker/api/expenses/ExpensesDetailsController.java
+++ src/main/java/learn/myapps/expensestracker/api/expenses/ExpensesDetailsController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api/v1/model")
+@RequestMapping("/api/v1/expenses")
 @RestController
 public class ExpensesDetailsController {
 
